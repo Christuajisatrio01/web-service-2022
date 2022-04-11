f30dcf989788e5a9249e1fcd59f2fb2be929d434
Christuajisatrio01 <satriobastano@gmail.com>
2022-04-11 13:18:40 +0800

    tugas 3 menampilkan dan menambah data blog

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\BlogController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -46,3 +49,18 @@ Route::view('welcome', 'welcome');
 route::get('/mahasiswa/{nama}', function ($nama){
     return "Nama Mahasiswa : $nama";
 });
+
+Route::view('master','template/master');
+
+//route untuk mahasiswa
+Route::get('data-mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('add-mahasiswa', [MahasiswaController::class, 'create']);
+Route::post('getData',[MahasiswaController::class,'ambilData'])->name('mahasiswa.getData');
+
+//Tugas
+Route::get('data-blog', [BlogController::class, 'index']);
+Route::get('add-blog', [BlogController::class, 'create']);
+Route::post('getData',[BlogController::class,'ambilData'])->name('blog.getData');
+
+
+
